eb6ae334cfe3bd3079a47ecfe25e7ceb645649e2
TheRealVDevelopers <runfast060425@gmail.com>
2026-02-17 11:44:00 +0530

    Revert "ok"
    This reverts commit 44f94493972579e29e16f6bd0c4531abd349e097.

--- dist/middleware/auth.d.ts
+++ dist/middleware/auth.d.ts
@@ -3,5 +3,5 @@ import { JwtPayload } from '../shared/utils/jwt';
 export interface AuthenticatedRequest extends Request {
     user?: JwtPayload;
 }
-export declare function authenticateJWT(req: AuthenticatedRequest, res: Response, next: NextFunction): void;
+export declare function authenticateJWT(req: AuthenticatedRequest, res: Response, next: NextFunction): void | Response<any, Record<string, any>>;
 //# sourceMappingURL=auth.d.ts.map
